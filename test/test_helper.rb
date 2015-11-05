b1fd864451e048c1220edf0524ce13cdfa279cb5
Chris Mytton <chrism@mysociety.org>
2015-11-05 13:36:09 +0000

    Require wikisnakker first in the tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
+require 'wikisnakker'
+
 require 'minitest/autorun'
 require 'minitest/around'
 require 'minitest/around/spec'
 require 'vcr'
-
-require 'wikisnakker'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'test/vcr_cassettes'
