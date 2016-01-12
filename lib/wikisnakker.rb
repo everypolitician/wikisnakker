e3491ee4ad7467a5e66299f9b79c2fa46a20c6ec
Chris Mytton <chrism@mysociety.org>
2016-01-12 11:57:29 +0000

    Add Wikisnakker::Sitelink class
    This allows us to represent the sitelinks as a domain object so we can
    potentially attach other methods to it in the future.

--- lib/wikisnakker.rb
+++ lib/wikisnakker.rb
@@ -209,4 +209,16 @@ module Wikisnakker
       end
     end
   end
+
+  class Sitelink
+    attr_reader :site
+    attr_reader :title
+    attr_reader :badges
+
+    def initialize(raw)
+      @site = raw['site']
+      @title = raw['title']
+      @badges = raw['badges']
+    end
+  end
 end
