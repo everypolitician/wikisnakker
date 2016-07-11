c13316b4e14b94b8bab63dcd44eacd63ddc94679
Chris Mytton <chrism@mysociety.org>
2016-07-11 14:36:09 +0100

    Sort properties by the claim's ranking
    When returning properties make sure they are returned in order of
    ranking.

--- test/ranking_test.rb
+++ test/ranking_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 describe 'fields ordered by rank' do
+  around { |test| VCR.use_cassette('ranking', &test) }
+
   it 'allows you to access the rank of the claim' do
     claim = Wikisnakker::Claim.new('rank' => 'preferred')
     claim.rank.must_equal('preferred')
+  end
+
+  it 'returns the highest ranked field by default' do
+    item = Wikisnakker::Item.find('Q401383')
+    item.P569.value.must_equal('1966-05-02')
   end
 end
