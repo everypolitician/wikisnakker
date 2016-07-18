740847e7a73de16b6f6ef9f510ab2cabb7f116a5
Chris Mytton <chrism@mysociety.org>
2016-07-18 18:45:39 +0200

    Add a more detailed test for sorting claims by rank
    I couldn't find a real-world example of a wikidata item that had all
    three rankings and more than one value for a given ranking, so instead
    I'm just interacting with the class directly to cover the necessary
    cases.

--- test/ranking_test.rb
+++ test/ranking_test.rb
@@ -4,12 +4,23 @@ describe 'fields ordered by rank' do
   around { |test| VCR.use_cassette('ranking', &test) }
 
   it 'allows you to access the rank of the claim' do
-    claim = Wikisnakker::Claim.new('rank' => 'preferred')
+    claim = Wikisnakker::Claim.new(rank: 'preferred')
     claim.rank.must_equal('preferred')
   end
 
   it 'returns the highest ranked field by default' do
-    item = Wikisnakker::Item.find('Q401383')
-    item.P569.value.must_equal('1966-05-02')
+    item = Wikisnakker::Item.new(
+      sitelinks: [],
+      claims: {
+        P1: [
+          { rank: 'normal', mainsnak: { datatype: 'string', datavalue: { value: 'normal rank 1' } } },
+          { rank: 'normal', mainsnak: { datatype: 'string', datavalue: { value: 'normal rank 2' } } },
+          { rank: 'preferred', mainsnak: { datatype: 'string', datavalue: { value: 'preferred rank' } } },
+          { rank: 'deprecated', mainsnak: { datatype: 'string', datavalue: { value: 'deprecated rank' } } },
+        ],
+      }
+    )
+    item.P1.value.must_equal('preferred rank')
+    item.P1s.map(&:value).must_equal(['preferred rank', 'normal rank 1', 'normal rank 2', 'deprecated rank'])
   end
 end
